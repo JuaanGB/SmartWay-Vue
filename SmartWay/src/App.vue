92aa46d24724a7268e9ac37b66defd274d941e48
JuaanGB <juanalejandro.gonzalezb@um.es>
2025-06-19 10:48:17 +0200

    Edición de tareas.
    - Muestra condicionalmente un h2/p o textareas en función de si el modo de edición está activo para la tarjeta (es por facilidad de varias líneas de titulo y descripcion).
    - Atributo de tarjeta 'completa' que hace binding condicional de clase para poner un estilo en la tarjeta u otro.
    - Tengo el problema de que las textarea usan v-model -> aunque no clique "guardar" se conservan los cambios.
    - Estilos con DaisyUI (lo he usado poquito de momento: tarjetas únicamente)

--- SmartWay/src/App.vue
+++ SmartWay/src/App.vue
@@ -5,10 +5,12 @@ import FormularioNuevaTarea from './components/FormularioNuevaTarea.vue';
 
 let id=1
 const tarjetas = ref([
-  {id: id++, titulo: "Titulo tarjeta 1", descripcion: "descripcion 1"},
-  {id: id++, titulo: "Titulo tarjeta 2", descripcion: "descripcion segunda tarjeta"},
-  {id: id++, titulo: "x", descripcion: "x"}
+  {id: id++, titulo: "Titulo tarjeta 1", descripcion: "descripcion 1", completa: false},
+  {id: id++, titulo: "Titulo tarjeta 2", descripcion: "descripcion segunda tarjeta", completa: true},
+  {id: id++, titulo: "x", descripcion: "x", completa: false}
 ])
+
+console.log(tarjetas)
 
 const text = ref('');
 const log = ref('Log de acciones: \n');
@@ -38,14 +40,28 @@ function anadirTarea(titulo, descripcion) {
   tarjetas.value.push({id: id++, titulo: titulo, descripcion: descripcion})
 }
 
+function guardarCambios({id, titulo, descripcion}) {
+  let tarea = tarjetas.value.find( t => t.id === id)
+  if (tarea) {
+    tarea.titulo = titulo
+    tarea.descripcion = descripcion
+  }
+}
+
 </script>
 
 <template>
-  <main class="flex flex-col gap-2 m-auto clase">
+  <main class="flex flex-col gap-2">
     <FormularioNuevaTarea @anadir-tarea="(titulo, descripcion) => anadirTarea(titulo, descripcion)"></FormularioNuevaTarea>
     <input v-model.trim="text" type="text" placeholder="Busca por título" class="bg-gray-200">
     <p>Buscando tarjetas con titulo: {{ text }}</p>
-    <TarjetaTarea @borrar-tarea="(id) => borrarTareaPorId(id)" v-for="(tarj, index) in tarjetasFiltradasTitulo" :id="tarj.id" :titulo="tarj.titulo" :descripcion="tarj.descripcion" :key="index"></TarjetaTarea>
+    <TarjetaTarea 
+      @borrar-tarea="(id) => borrarTareaPorId(id)" 
+      @guardar-cambios="guardarCambios"
+      v-for="(tarj, index) in tarjetasFiltradasTitulo" 
+      :id="tarj.id" :titulo="tarj.titulo" :descripcion="tarj.descripcion" :key="tarj.id" :completa="tarj.completa"
+      :class="{ 'tarea-hecha': tarj.completa}"
+    ></TarjetaTarea>
   </main>
   <span id="log" class="bg-gray-200 whitespace-pre-line">{{ log }}</span>
 </template>
@@ -54,4 +70,5 @@ function anadirTarea(titulo, descripcion) {
 /* A todos los componentes incluidos dentro del componente App podrán utilizar los estilos de <style>
 Si quiero estilos privados al componente, emplear <style scoped> */
 @import 'tailwindcss';
+@plugin 'daisyui';
 </style>
--- SmartWay/src/components/TarjetaTarea.vue
+++ SmartWay/src/components/TarjetaTarea.vue
@@ -1,31 +1,65 @@
 <script setup>
     import { ref } from 'vue'
 
-    defineProps(["id", "titulo", "descripcion"])
-    let editEnable = ref(true)
+    const props = defineProps(["id", "titulo", "descripcion", "completa"])
+
+    let id = props.id
+    let titulo = ref(props.titulo)
+    let descripcion = ref(props.descripcion)
+    let completa = ref(!props.completa)
+
+    let editMode = ref(false)
+
     function tareaHecha(event) {
         const tarjeta = event.target.closest("article")
         if (tarjeta.classList.contains("tarea-hecha"))
             tarjeta.classList.remove("tarea-hecha")
         else
             tarjeta.classList.add("tarea-hecha")
-        editEnable.value = !editEnable.value;
+        completa.value = !completa.value;
+    }
+
+    function activarEdicion(event) {
+        editMode.value = !editMode.value;
     }
 </script>
 
 
 <template>
-    <article class="p-5 gap-y-4 bg-red-200 p-4 max-w-[400px] rounded-lg shadow-md grid grid-cols-[1fr,auto,auto] grid-rows-[auto,auto] gap-2 items-start">
-        <h2 class="text-xl ont-semibold col-span-1 row-start-1 row-end-2" @click="tareaHecha($event)">{{ titulo }}</h2>
-        <button :disabled="!editEnable" class="btn btn-sm bg-gray-200 row-start-1 col-start-2 btn rounded-lg shadow-sm">Editar</button>
-        <button :disabled="!editEnable" @click="$emit('borrarTarea', id)" class="btn btn-sm bg-gray-200 row-start-1 col-start-3 btn rounded-lg shadow-sm">X</button>
-        <p class="col-span-3 row-start-2 text-gray-700 text-wrap">{{ descripcion }}</p>
+    <article class="card bg-red-100 p-4 shadow-sm w-1/4">
+        <div class="card-title">
+            <h2 v-if="!editMode" @click="tareaHecha($event)">{{ titulo }}</h2>
+            <textarea v-else :disabled="!editMode" :value="titulo" v-model="titulo" class="enabled:border p-1 w-auto whitespace-break-spaces"></textarea>
+            <button class="btn btn-sm btn-circle text-xl ml-auto mb-auto shadow-sm"
+                :disabled="!completa"
+                @click="activarEdicion($event)"
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><!-- Icon from HeroIcons by Refactoring UI Inc - https://github.com/tailwindlabs/heroicons/blob/master/LICENSE --><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m16.862 4.487l1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8l.8-2.685a4.5 4.5 0 0 1 1.13-1.897zm0 0L19.5 7.125"/></svg>
+            </button>
+            <button class="btn btn-sm btn-circle text-xl mb-auto shadow-sm"
+                @click="console.log(id); $emit('borrarTarea', id)"
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><!-- Icon from HeroIcons by Refactoring UI Inc - https://github.com/tailwindlabs/heroicons/blob/master/LICENSE --><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21q.512.078 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48 48 0 0 0-3.478-.397m-12 .562q.51-.088 1.022-.165m0 0a48 48 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a52 52 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a49 49 0 0 0-7.5 0"/></svg>
+            </button>
+        </div>
+        <div class="card-body p-0 pt-2">
+            <p v-if="!editMode">{{ descripcion }}</p>
+            <textarea v-else :disabled="!editMode" :value="descripcion" v-model="descripcion" class="enabled:border p-1 w-auto"></textarea>
+        </div>
+        <div class="card-actions">
+            <button v-show="editMode" class="btn ml-auto mt-2"
+                @click="$emit('guardarCambios', { id, titulo: titulo.value, descripcion: descripcion.value }); editMode=!editMode">Guardar</button>
+        </div>
     </article>
 </template>
 
 <style scoped>
 h2:hover {
     text-decoration: line-through;
+}
+
+article.tarea-hecha {
+    background-color: green; /* Cómo usar clases Tailwind aquí? */
 }
 
 article.tarea-hecha h2, article.tarea-hecha p {
